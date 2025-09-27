3c795a84e450a88e0692f71fd11acb33a5296584
gabemods <gabriel.longshaw@gmail.com>
2025-09-27 19:28:18 +0100

    feat: design overhall

--- assets/js/wave-text.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.wave-text').forEach(function(p) {
-  p.innerHTML = p.innerText.split(' ').map(function(word) {
-    return '<span>' + word + '</span>';
-  }).join(' ');
-});
