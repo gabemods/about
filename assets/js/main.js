75854ca4545c9b92717d239dfc28047071e129f9
gabemods <gabriel.longshaw@gmail.com>
2025-10-01 18:54:48 +0100

    feat: new design

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,22 @@ prefersDark.addEventListener('change', applySystemTheme);
 
 const yearSpan = document.getElementById('year');
 if (yearSpan) yearSpan.textContent = new Date().getFullYear();
+
+function setupInfiniteScroll(scrollerInnerSelector) {
+  const scrollerInner = document.querySelector(scrollerInnerSelector);
+  
+  if (!scrollerInner) return;
+  
+  const children = Array.from(scrollerInner.children);
+  
+  children.forEach(item => {
+    
+    const clonedItem = item.cloneNode(true);
+    clonedItem.setAttribute('aria-hidden', 'true');
+    scrollerInner.appendChild(clonedItem);
+  });
+}
+
+setupInfiniteScroll('.projects-row-1 .scroller-inner');
+setupInfiniteScroll('.projects-row-2 .scroller-inner');
+
